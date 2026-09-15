d5a7ace9d6b01cef20a33a17b18a4bf8e586be2c
ValtaLab <isearover@gmail.com>
2026-09-16 00:09:13 +0800

    feat: 手機版改用網格線分隔風格，取代一日一卡片（內容寬度 +20%）

--- app.js
+++ app.js
@@ -7,8 +7,8 @@
   'use strict';
 
   // 由 bump-version.sh 自動維護
-  const APP_VERSION = '1.6.0';
-  const APP_BUILD = '20260915-2317';
+  const APP_VERSION = '1.7.1';
+  const APP_BUILD = '20260916-0006';
 
   const STORE_KEY = 'worktime-calendar:v1';
   const SETTINGS_KEY = 'worktime-calendar:settings:v1';
